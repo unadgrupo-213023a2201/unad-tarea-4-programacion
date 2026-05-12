65e879472bc7f3df526d2e0cc4bbfb39b508a63c
Andres Julio <andresjulio011@gmail.com>
2026-05-12 11:07:17 -0500

    Manejo de exepciones - Andres Julio

--- Excepciones/excepciones.py
+++ Excepciones/excepciones.py
@@ -1,11 +1,12 @@
-#Control de errores 
+# Nombre del script: Manejo de exepciones
+# Autor: Andres Alfonso Julio Hernandez
+
 class ClienteError(Exception):
     pass
 
 
 class ServicioError(Exception):
     pass
-
 
 class ReservaError(Exception):
     pass
